a791ae05b63f232dbc0f571d8f6b88cbd98eb721
yubin <kimu2371@naver.com>
2023-10-07 16:05:35 +0900

    feat: Make Timer UI draft

--- app/src/main/java/com/gogumac/devpulse/MainActivity.kt
+++ app/src/main/java/com/gogumac/devpulse/MainActivity.kt
@@ -3,12 +3,16 @@ package com.gogumac.devpulse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.gogumac.devpulse.ui.theme.DevPulseTheme
 
@@ -18,10 +22,22 @@ class MainActivity : ComponentActivity() {
         setContent {
             DevPulseTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TimeTrackScreen()
                 }
             }
         }
+    }
+}
+
+
+@Preview
+@Composable
+fun Preview() {
+    DevPulseTheme {
+        TimeTrackScreen()
     }
 }
