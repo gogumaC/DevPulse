6f7e9bf939e3c17053d5cf835339879bcb7e2ecb
yubin <kimu2371@naver.com>
2023-10-08 14:31:13 +0900

    feat: Implement timer Activated state

--- app/src/main/java/com/gogumac/devpulse/TimeTrackScreen.kt
+++ app/src/main/java/com/gogumac/devpulse/TimeTrackScreen.kt
@@ -7,32 +7,42 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.gogumac.devpulse.ui.theme.DevPulseTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
 
 @Composable
 fun TimeTrackScreen(modifier: Modifier = Modifier) {
+
+    var isActivated by remember { mutableStateOf(false) }
+
     Column(modifier = modifier.fillMaxSize()) {
         TimeIndicator()
         IconButton(
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
                 .size(50.dp),
-            onClick = { /*TODO*/ },
+            onClick = { isActivated = !isActivated },
         ) {
+            val icon = if (isActivated) Icons.Default.Done else Icons.Default.PlayArrow
             Icon(
                 modifier = Modifier.fillMaxSize(),
-                imageVector = Icons.Default.PlayArrow,
+                imageVector = icon,
                 contentDescription = null
             )
         }
